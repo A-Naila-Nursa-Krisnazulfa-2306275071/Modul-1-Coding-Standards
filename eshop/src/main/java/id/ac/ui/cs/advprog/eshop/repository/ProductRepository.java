342948cf6304bd706be112788ffd741a406600a7
nairisfa <nailakrisnazulfa@gmail.com>
2026-02-08 13:14:11 +0700

    Updating Product Repository for Edit

--- eshop/src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ eshop/src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -19,4 +19,25 @@ public class ProductRepository {
     public Iterator<Product> findAll() {
         return productData.iterator();
     }
+
+    public Product findProductById(String id) {
+        for (Product product : productData) {
+            if (product.getProductId().equals(id)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Product updateProduct(Product updatedProduct) {
+        Product existingProduct = findProductById(updatedProduct.getProductId());
+
+        if (existingProduct != null) {
+            existingProduct.setProductName(updatedProduct.getProductName());
+            existingProduct.setProductQuantity(updatedProduct.getProductQuantity());
+            existingProduct.setProductId(updatedProduct.getProductId());
+            return existingProduct;
+        }
+        return null;
+    }
 }
