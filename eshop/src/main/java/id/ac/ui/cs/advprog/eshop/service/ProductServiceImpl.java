98a83709c07c9088e6d3546bb0660d6e9fbdab25
nairisfa <nailakrisnazulfa@gmail.com>
2026-02-08 15:08:55 +0700

    Updating Product Service Implementation for Delete

--- eshop/src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ eshop/src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -38,4 +38,9 @@ public class ProductServiceImpl implements ProductService {
      public Product updateProduct(Product product) {
          return productRepository.updateProduct(product);
      }
+
+     @Override
+     public void deleteProductById(String id) {
+         productRepository.deleteProductById(id);
+     }
 }
